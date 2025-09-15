ac6d7e8266965d1bdc9d05ed9bbc18bbc27bf924
sachinshrmaa <krisa1826@gmail.com>
2025-09-15 11:50:28 +0530

    add client side routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import About from "../src/pages/about";
+import Contact from "../src/pages/contact";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        {/* Route 1: The About page */}
+        <Route path="/about" element={<About />} />
+
+        {/* Route 2: The Contact page */}
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
---
+++ src/pages/about.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi atque
+        dolorem, hic quod dolore illum.
+      </p>
+    </div>
+  );
+}
